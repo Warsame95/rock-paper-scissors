1b64c121b914a45b92aedd9ce387b66c406e6d03
Warsame <warsameahmed95@gmail.com>
2022-08-15 20:27:36 +0100

    Implemented playRound function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice(){
-    
+
     const options = ["Rock", "Paper", "Scissors"];
 
     let index = Math.floor(Math.random() * 3);
@@ -7,5 +7,52 @@ function getComputerChoice(){
     let choice = options[index];
 
     return choice;
+
+}
+
+function playRound(playerSelection, computerSelection){
+    let outcome;
+    if (playerSelection.toLowerCase() == "rock"){
+        switch(computerSelection.toLowerCase()){
+            case "rock":
+                outcome = "Both picked Rock. It's a Draw";
+                break;
+            case "paper":
+                outcome = "You Lose! Paper beats Rock";
+                break;
+            case "scissors":
+                outcome = "You Win! Rock beats Scissor";
+                break;
+        }
+    }
+
+    else if (playerSelection.toLowerCase() == "paper"){
+        switch(computerSelection.toLowerCase()){
+            case "rock":
+                outcome = "You Win! Paper beats Rock";
+                break;
+            case "paper":
+                outcome = "Both picked Paper. It's a Draw";
+                break;
+            case "scissors":
+                outcome = "You Lose! Scissor beats Paper";
+                break;
+        }
+    }
+
+    else {
+        switch(computerSelection.toLowerCase()){
+            case "rock":
+                outcome = "You Lose! Rock beats Scissors";
+                break;
+            case "paper":
+                outcome = "You Win! Scissor beats Paper";
+                break;
+            case "scissors":
+                outcome = "Both picked Scissor. It's a Draw";
+                break;
+        }
+    }
+    return outcome;
 
 }
